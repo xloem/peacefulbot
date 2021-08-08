89322f4f44d9f82dd299747bb61b6c5c23c47205
xloem <0xloem@gmail.com>
2021-08-08 11:48:33 +0000

    committing to transfer work easily

---
+++ feelings_needs.py
@@ -0,0 +1,141 @@
+feeling_words_by_category = {
+    'powerful': 'proud cheerful respected satisfied appreciated valuable hopeful worthwhile important intelligent faithful confident'.split(' '),
+    'peaceful': 'nurturing thankful trusting sentimental loving serene intimate responsive thoughtful relaxed content pensive'.split(' '),
+    'sad': 'guilty bashful ashamed stupid depressed miserable lonely inadequate bored inferior sleepy apathetic'.split(' '),
+    'mad': 'hurt jealous hostile selfish angry frustrated rage furious hateful irritated critical skeptical'.split(' '),
+    'scared': 'rejected bewildered confused discouraged helpless insignificant submissive insecure anxious embarrassed'.split(' '),
+    # diverse tones. the real meaning of words is what the listener feels when they are said.
+    'joyful': 'excited daring sexy fascinating energetic stimulating playful amused creative extravagant aware delightful'.split(' '),
+}
+
+# these needs are from a dichotomously-organised nvc needs wheel.
+needs_structured = {
+   'life': {
+       'happiness': {
+           'inspiration': ('appreciation', 'beauty'),
+           'joy': ('humor', 'delight'),
+       },
+       'gratefulness': {
+           'thankfulness': ('celebration', 'gratitude'),
+           'remembrance': ('mourning', 'grieving'),
+       },
+   },
+   'mutuality': {
+       'collaboration': {
+           'contribution': ('care', 'giving', 'nurturance'),
+           'cooperation': ('help', 'support', 'participation'),
+       },
+       'relationship': {
+           'closeness': ('intimacy', 'affection'),
+           'community': ('inclusion', 'belonging'),
+       },
+   },
+   'worth': {
+       'to matter': {
+           'acknowledgement': ('appreciation', 'attention'),
+           'respect': ('admiration', 'value'),
+       },
+       'esteem': {
+           'achievement': ('success', 'accomplishment'),
+           'recognition': ('validation', 'affirmation'),
+       },
+   },
+   'identity': {
+       'uniqueness': {
+           'expression': ('creativity', 'originality'),
+           'extraordinariness': ('to be known', 'specialness'),
+       },
+       'authenticity': {
+           'genuineness': ('congruence', 'sincerity'),
+           'significance': ('purpose', 'meaning'),
+       },
+   },
+   'love': {
+       'empathy': {
+           'understanding': ('presence', 'patience'),
+           'compassion': ('kindness', 'consideration'),
+       },
+       'connection': {
+           'openness': ('tolerance', 'acceptance'),
+           'relatedness': ('receptivity', 'sincerity'),
+       },
+   },
+   'self-efficiency': {
+       'awareness': {
+           'wisdom': ('clarity', 'curiosity'),
+           'knowledge': ('learning', 'growth'),
+       },
+       'agency': {
+           'capability': ('skill', 'competence'),
+           'confidence': ('effectiveness', 'proficiency'),
+       },
+   },
+   'trust': {
+           'assurance': {
+               'dependability': ('commitment', 'loyalty'),
+               'predictability': ('consistency', 'reliability'),
+           },
+           'safety': {
+               'stability': ('structure', 'order'),
+               'protection': ('preparedness', 'security'),
+           },
+   },
+   'fulfillment': {
+       'experience': {
+           'adventure': ('discovery', 'exploration'),
+           'stimulation': ('opportunity', 'challenge'),
+       },
+       'aliveness': {
+           'richness': ('variety', 'excitement'),
+           'enjoyment': ('spontaneity', 'fun', 'play'),
+       },
+   },
+   'well-being': {
+       'sustenance': {
+           'vitality': ('liveliness', 'vivacity'),
+           'energy': ('nourishment', 'nutrition'),
+       },
+       'health': {
+           'self-care': ('comfort', 'relief'),
+           'rejuvenation': ('rest', 'relaxation', 'healing'),
+       },
+   },
+   'autonomy': {
+       'independence': {
+           'freedom': ('choice', 'self-determination'),
+           'separateness': ('space', 'privacy', 'boundaries'),
+       },
+       'individuality': {
+           'strength': ('courage', 'resolve'),
+           'leadership': ('decisiveness', 'influence'),
+       },
+   },
+   'harmony': {
+       'acceptance': {
+           'tolerance': ('modesty', 'humility'),
+           'fluidity': ('convenience', 'ease'),
+       },
+       'peace': {
+           'balance': ('serenity', 'calmness'),
+           'unity': ('consensus', 'agreement'),
+       },
+   },
+   'wholeness': {
+       'integrity': {
+           'truthfulness': ('honesty', 'goodness'),
+           'fairness': ('equality', 'justice'),
+       },
+       'responsibility': {
+           'accountability': ('right action', 'certitude'),
+           'civility': ('courteousness', 'politeness'),
+       },
+   },
+}
+
+feeling_category_by_word = {}
+for feeling_name, feeling_words in feeling_words_by_category.items():
+    feeling_category_by_word[feeling_name] = feeling_name
+    for feeling_word in feeling_words:
+        feeling_category_by_word[feeling_word] = feeling_name
+
+feeling_words = [*feeling_category_by_word.keys()]
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import transformers
 
 from time import time
 import models
+import feelings_needs
 
 # a thought:
 #   an organism that uses pretrained machine learning models
@@ -83,139 +84,9 @@ feelings = {
     'mad': -1,
 }
 
-feeling_words = {
-    'powerful': 'proud cheerful respected satisfied appreciated valuable hopeful worthwhile important intelligent faithful confident'.split(' '),
-    'peaceful': 'nurturing thankful trusting sentimental loving serene intimate responsive thoughtful relaxed content pensive'.split(' '),
-    'sad': 'guilty bashful ashamed stupid depressed miserable lonely inadequate bored inferior sleepy apathetic'.split(' '),
-    'mad': 'hurt jealous hostile selfish angry frustrated rage furious hateful irritated critical skeptical'.split(' '),
-    'scared': 'rejected bewildered confused discouraged helpless insignificant submissive insecure anxious embarrassed'.split(' '),
-    # diverse tones. the real meaning of words is what the listener feels when they are said.
-    'joyful': 'excited daring sexy fascinating energetic stimulating playful amused creative extravagant aware delightful'.split(' '),
-}
+def classify_feelings(phrases):
+    return classify(phrases, feelings_needs.feeling_words, 'This person is feeling {}.', multi_label = True)
 
-# these needs are from a dichotomously-organised nvc needs wheel.
-needs = {
-   'life': {
-       'happiness': {
-           'inspiration': ('appreciation', 'beauty'),
-           'joy': ('humor', 'delight'),
-       },
-       'gratefulness': {
-           'thankfulness': ('celebration', 'gratitude'),
-           'remembrance': ('mourning', 'grieving'),
-       },
-   },
-   'mutuality': {
-       'collaboration': {
-           'contribution': ('care', 'giving', 'nurturance'),
-           'cooperation': ('help', 'support', 'participation'),
-       },
-       'relationship': {
-           'closeness': ('intimacy', 'affection'),
-           'community': ('inclusion', 'belonging'),
-       },
-   },
-   'worth': {
-       'to matter': {
-           'acknowledgement': ('appreciation', 'attention'),
-           'respect': ('admiration', 'value'),
-       },
-       'esteem': {
-           'achievement': ('success', 'accomplishment'),
-           'recognition': ('validation', 'affirmation'),
-       },
-   },
-   'identity': {
-       'uniqueness': {
-           'expression': ('creativity', 'originality'),
-           'extraordinariness': ('to be known', 'specialness'),
-       },
-       'authenticity': {
-           'genuineness': ('congruence', 'sincerity'),
-           'significance': ('purpose', 'meaning'),
-       },
-   },
-   'love': {
-       'empathy': {
-           'understanding': ('presence', 'patience'),
-           'compassion': ('kindness', 'consideration'),
-       },
-       'connection': {
-           'openness': ('tolerance', 'acceptance'),
-           'relatedness': ('receptivity', 'sincerity'),
-       },
-   },
-   'self-efficiency': {
-       'awareness': {
-           'wisdom': ('clarity', 'curiosity'),
-           'knowledge': ('learning', 'growth'),
-       },
-       'agency': {
-           'capability': ('skill', 'competence'),
-           'confidence': ('effectiveness', 'proficiency'),
-       },
-   },
-   'trust': {
-           'assurance': {
-               'dependability': ('commitment', 'loyalty'),
-               'predictability': ('consistency', 'reliability'),
-           },
-           'safety': {
-               'stability': ('structure', 'order'),
-               'protection': ('preparedness', 'security'),
-           },
-   },
-   'fulfillment': {
-       'experience': {
-           'adventure': ('discovery', 'exploration'),
-           'stimulation': ('opportunity', 'challenge'),
-       },
-       'aliveness': {
-           'richness': ('variety', 'excitement'),
-           'enjoyment': ('spontaneity', 'fun', 'play'),
-       },
-   },
-   'well-being': {
-       'sustenance': {
-           'vitality': ('liveliness', 'vivacity'),
-           'energy': ('nourishment', 'nutrition'),
-       },
-       'health': {
-           'self-care': ('comfort', 'relief'),
-           'rejuvenation': ('rest', 'relaxation', 'healing'),
-       },
-   },
-   'autonomy': {
-       'independence': {
-           'freedom': ('choice', 'self-determination'),
-           'separateness': ('space', 'privacy', 'boundaries'),
-       },
-       'individuality': {
-           'strength': ('courage', 'resolve'),
-           'leadership': ('decisiveness', 'influence'),
-       },
-   },
-   'harmony': {
-       'acceptance': {
-           'tolerance': ('modesty', 'humility'),
-           'fluidity': ('convenience', 'ease'),
-       },
-       'peace': {
-           'balance': ('serenity', 'calmness'),
-           'unity': ('consensus', 'agreement'),
-       },
-   },
-   'wholeness': {
-       'integrity': {
-           'truthfulness': ('honesty', 'goodness'),
-           'fairness': ('equality', 'justice'),
-       },
-       'responsibility': {
-           'accountability': ('right action', 'certitude'),
-           'civility': ('courteousness', 'politeness'),
-       },
-   },
-}
 
 
 conversation = transformers.Conversation()
